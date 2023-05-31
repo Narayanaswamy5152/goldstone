fc6a4ad55e2706ae0ecfd3d6b2988056d320a160
Narayanaswamy5152 <79390400+Narayanaswamy5152@users.noreply.github.com>
2023-05-31 13:39:17 +0530

    Update index.js

--- src/components/DetailItem/index.js
+++ src/components/DetailItem/index.js
@@ -3,7 +3,7 @@ import './index.css'
 
 const DetailItem = props => {
   const {Details, toggleIsStarred} = props
-  const {name, id, email, gender, date, isStarred, status} = Details
+  const {name, id, email, gender, date, isStarred} = Details
   const starImgUrl = isStarred
     ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
@@ -12,19 +12,14 @@ const DetailItem = props => {
     toggleIsStarred(id)
   }
 
-  const isActiveClassName =
-    status === 'active' ? 'active-class' : 'inActive-class'
+ 
 
   return (
     <li className="detail-item">
       <div className="header-container">
         <div>
-          <div className="head-para">
-            <p className="title">Name: {name}</p>
-            <p className={isActiveClassName}>{status}</p>
-          </div>
-
-          <p className="title">Email: {email}</p>
+          <p className="title">Name: {name}</p>
+           <p className="title">Email: {email}</p>
           <p className="title">Gender:{gender}</p>
         </div>
         <button
